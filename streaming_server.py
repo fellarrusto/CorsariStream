476a0a4a31e0e16b27d52c257c24ca7d908029df
Fellarrusto <filippo.leonelli@linksmt.it>
2023-12-20 17:16:51 +0100

    Added camera select

--- streaming_server.py
+++ streaming_server.py
@@ -6,6 +6,42 @@ import time
 from waitress import serve
 import os
 
+# Function to scan and select the video source
+def select_camera():
+    max_cameras = 10
+    available_cameras = []
+    for i in range(max_cameras):
+        cap = cv2.VideoCapture(i)
+        if cap.read()[0]:
+            available_cameras.append(i)
+            cap.release()
+
+    if not available_cameras:
+        raise Exception("No cameras found.")
+
+    print("Available cameras:", available_cameras)
+    camera_index = int(input("Select camera index: "))
+    if camera_index not in available_cameras:
+        raise ValueError(f"Invalid camera index: {camera_index}")
+
+    return camera_index
+
+# Call the camera selection function before starting the server
+camera_index = select_camera()
+
+os.system('cls')
+print(r"""
+   _____                     _         _____ _                            
+  / ____|                   (_)       / ____| |                           
+ | |     ___  _ __ ___  __ _ _ _ __  | (___ | |_ _ __ ___  __ _ _ __ ___  
+ | |    / _ \| '__/ __|/ _` | | '__|  \___ \| __| '__/ _ \/ _` | '_ ` _ \ 
+ | |___| (_) | |  \__ \ (_| | | |     ____) | |_| | |  __/ (_| | | | | | |
+  \_____\___/|_|  |___/\__,_|_|_|    |_____/ \__|_|  \___|\__,_|_| |_| |_|
+          
+---------------------------STREAMING ON PORT 5000---------------------------
+""")
+
+
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
@@ -36,17 +72,5 @@ def index():
     return "<html><body><img src='/video_feed'></body></html>"
 
 if __name__ == '__main__':
-    # Enable threading in Flask
-    os.system('cls')
-    print(r"""
-   _____                     _         _____ _                            
-  / ____|                   (_)       / ____| |                           
- | |     ___  _ __ ___  __ _ _ _ __  | (___ | |_ _ __ ___  __ _ _ __ ___  
- | |    / _ \| '__/ __|/ _` | | '__|  \___ \| __| '__/ _ \/ _` | '_ ` _ \ 
- | |___| (_) | |  \__ \ (_| | | |     ____) | |_| | |  __/ (_| | | | | | |
-  \_____\___/|_|  |___/\__,_|_|_|    |_____/ \__|_|  \___|\__,_|_| |_| |_|
-          
----------------------------STREAMING ON PORT 5000---------------------------
-""")
     serve(app, host='0.0.0.0', port=5000)
     
